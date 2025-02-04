bff67d0d2270b0e5194171078c3e0f21e39a814c
Elijah Moye <elijahmoye96@gmail.com>
2025-02-04 01:10:50 -0500

    yeah

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '/Users/elijahmoye/Desktop/battleship/battleship/src/css/style.css'
+import '/Volumes/My Code/battleship/battleship/src/css/style.css'
 import { Battleship } from "./gameLogic/gameFunctions";
 import { Ships } from "./gameLogic/gamePeices";
 import { createDom } from './Ui/ui';
